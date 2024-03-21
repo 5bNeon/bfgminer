2e8b2126925d3f00b36f0680003e43aa72fb6fd9
5bNeon <93319161+5bNeon@users.noreply.github.com>
2024-03-22 00:01:22 +0800

    Update miner.c

--- miner.c
+++ miner.c
@@ -9,6 +9,7 @@
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option)
  * any later version.  See COPYING for more details.
+ * zy Wang changed
  */
 
 #include "config.h"
